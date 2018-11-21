ab6d93cdf9381cad1b65671b6cbe412cd05cc134
Yuri Galiza G Lyra <yuri.lyra.external@atos.net>
2018-11-21 19:01:25 -0200

    Enhanced the test

--- src/test/java/com/gallery/starter/GalleryApplicationTests.java
+++ src/test/java/com/gallery/starter/GalleryApplicationTests.java
@@ -4,19 +4,31 @@ import com.gallery.model.Login;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.Assert.*;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 @SpringBootTest
 public class GalleryApplicationTests {
 
     @Autowired
-    private WebTestClient webClient;
+    private MockMvc mvc;
 
+//    @Autowired
+//    private WebTestClient webClient;
     @Test
-    public void loginTestConnection() {
-        this.webClient.post().uri("/login").exchange().expectStatus().isOk();
+    public void loginTestConnection() throws Exception {
+        Login login = new Login("y@y", "p");
+        this.mvc.perform(post("/login").sessionAttr("login", login)
+                .param("email", login.getEmail())
+                .param("password", login.getPassword())
+        ).andExpect(status().isOk());
     }
 }
